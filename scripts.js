0e6ff4ffe4f38f28336b0d8f68bb042e04fc8a4b
uzairjacobs <93554483+uzairjacobs@users.noreply.github.com>
2023-08-06 22:41:29 +0200

    fixed code

--- scripts.js
+++ scripts.js
@@ -1,31 +1,33 @@
-let date = 2050;
-let status = "parent";
-let count = 0;
+const date = 2050
+const status = "parent"
+let count = 0
 
-if ((date = 2050)) {
-  console.log("January", "New Years Day");
-  console.log("March", "Human Rights Day");
-  month = "April";
-  console.log(month, "Family Day");
-  console.log(month, "Freedom Day");
-  count = count + 4;
+if (date === 2050) {
+	console.log("January", 'New Years Day');
+	console.log("March", 'Human Rights Day');
+	month = 'April'
+	console.log(month, 'Family Day')
+	console.log(month, 'Freedom Day')
+	count = count + 4
 
-  if ((status = "student")) {
-    console.log("June", "Youth Day");
-    console.log("August", "Women’s Day");
-    console.log("September", "Heritage Day");
-    month = "December";
-    console.log(month, "Day of Reconciliation");
-    count = count + 4;
-  }
 
-  if ((status = "parent")) {
-    console.log(month, "Christmas Day");
-    console.log(month, "Day of Goodwill");
-    count = count + 2;
-  }
+	if (status === "student") {
+	  console.log('June', 'Youth Day')
+		console.log('August', 'Women’s Day')
+	  console.log('September', 'Heritage Day')
+	  month = 'December'
+	  console.log(month, 'Day of Reconciliation')
+	 count = count + 4
+    }
+
+	if  (status === "parent") {
+    month = 'December'
+	  console.log(month, 'Christmas Day')
+    console.log(month, 'Day of Goodwill')
+	  count = count + 2
+    }
 }
 
-console.log("Your status is:", status);
-console.log("The year is:", date);
-console.log("The total holidays is:", count);
+console.log('Your status is:', status)
+console.log('The year is:', date)
+console.log('The total holidays is:', count)
